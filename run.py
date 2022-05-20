744a05dc86e34f8906fdb4f5b1d12612027c2a45
mihayounas <mihaelayounas55@gmail.com>
2022-05-20 21:34:15 +0000

    Add calendar apis

--- run.py
+++ run.py
@@ -6,8 +6,10 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/calendar"
     ]
-
+API_NAME = 'calendar'
+API_VERSION = 'v3'
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
