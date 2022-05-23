2ffda6c6e4cdac76e803cddd0c81cfc5a5369bb5
mihayounas <mihaelayounas55@gmail.com>
2022-05-23 10:19:58 +0000

    Add changes to the function and get user details

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive",
-    "http://www.googleapis.com/auth/calendar"
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
@@ -23,11 +21,9 @@ def get_details_data():
     print('Welcome to My Virtual Doctor !')
     print('\n An app which helps you book your doctor appointments fast!\n')
     print('To use this app, hit enter after each choice.\n')
-    print('After confirming all your details you will receive\n')
-    print('a confirmation email!\n')
     
-    data_str = input("Please Enter Your Full Name here:\n")
-    print(f"Hello {data_str}\n")
+    data_str = input("Please Enter Your Full Name here(space between):")
+    print(f"Hello {data_str}")
 
 
 get_details_data()
