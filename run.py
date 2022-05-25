8dc96d3da5325d2abe4e6cf8deb836beaf07ded6
mihayounas <mihaelayounas55@gmail.com>
2022-05-25 14:06:10 +0000

    Add a separate welcome function

--- run.py
+++ run.py
@@ -14,18 +14,36 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("my_virtual_doctor")
 
 
+def welcome_message():
+    print('Welcome to My Virtual Doctor !')
+    print('\n An app which helps you book your doctor appointments fast!\n')
+    print('To use this app, press enter after each choice.\n')
+    print('After confirming all your details you will receive\n')
+    print('a confirmation email!\n')
+
+    while True:
+        admin_or_patient = input(
+            'Please press "r" to register an appointment or "a"'
+            'for admin area:\n\n'
+        )
+        admin_or_patient = admin_or_patient.lower()
+
+        if admin_or_patient == 'r':
+            get_details_data()
+            return False
+
+        print('\nInvalid entry, please try again\n')
+
+
+welcome_message()
+
+
 def get_details_data():
     """
     Get input details from the customer.
     Get Full Name and date of birth.
     Displaying the age of the customer.
     """
-
-    print('Welcome to My Virtual Doctor !')
-    print('\n An app which helps you book your doctor appointments fast!\n')
-    print('To use this app, press enter after each choice.\n')
-    print('After confirming all your details you will receive\n')
-    print('a confirmation email!\n')
     name = input('Please enter your full name :\n')
     try:
         name.split()[1]
@@ -51,4 +69,5 @@ def get_details_data():
     print(f"You are {int(age_years)} years old")
    
 
-get_details_data()
+
+
