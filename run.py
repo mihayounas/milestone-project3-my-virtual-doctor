26c267f083d628e516e6e1145986a3ca8d0faf75
mihayounas <mihaelayounas55@gmail.com>
2022-05-25 17:15:16 +0000

    Add main function to gather all the functions and update the work sheets

--- run.py
+++ run.py
@@ -67,14 +67,27 @@ def get_details_data():
     convertdays = int(age.days)
     age_years = convertdays/365
     print(f"You are {int(age_years)} years old")
+    inputEmail = input("Please enter a valid email: \n")
+    
+    get_symptoms()
+
+
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of strings to be inserted into worksheet
+    Update the relevant worksheet with the data provided
+    """
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} data saved succesfully\n")
 
 
 def confirm_terms():
     """
     Function which will let patient decide either to continue 
     with appointment or go back,for example if admin was to log in
-    but by mistake pressed "r" or the user doesn't agree to the terms of
-    saving of data.
+    but by mistake pressed "r" or the user doesn't agree to the 
+    terms of saving of data.
     """
     print('Please note we are saving your details in our database.')
     print('If you agree to the terms plese confirm by pressing')
@@ -88,6 +101,61 @@ def confirm_terms():
         welcome_message()
 
 
-welcome_message()
-confirm_terms()
-get_details_data()
+def get_symptoms():
+    """
+    COLLECTS a personalised message from the patient 
+    describing their situation and their symptoms
+    """
+    symptoms = input("Please enter you symptoms bellow :\n")
+
+    if len(symptoms) < 6:
+        print("Please add more details for your doctor.")
+        get_symptoms()
+    else: 
+        print("Thank you for your details,a confirmation email will follow.")
+        menu_exit = input("Please press 'm' for main menu or 'e' to exit...\n")
+        if menu_exit == "m":
+            welcome_message()
+        else:
+            exit_screen()
+
+
+def exit_screen():
+    print("Thank you for visiting our application !")
+    print("What would you like to do next ?")
+    exit = input("To manage your appointments press '1' or 'e' to close : \n")
+    if exit == "1":
+        confirmation_data()
+    else:
+        print("Thank you for your appoinment!")
+   
+
+def confirmation_data():
+    """
+    Will diplay all the details of the patient and 
+    show the date he has an appoinment, an option to cancel
+    or ammend this booking.
+    """
+    name = " "
+    inputDate = " "
+    inputEmail = " "
+    symptoms = " "
+    print(f"Name : {name}")
+    print(f"DOB : {inputDate}")
+    print(f"Email : {inputEmail}")
+    print(f"Your message :{symptoms}")
+
+
+def main():
+    """
+    Run all program functions
+    """
+    welcome_message()
+    data = get_details_data()
+    details_data = []
+    update_worksheet(details_data, details)
+    confirm_terms()
+    get_symptoms()
+
+
+main()
