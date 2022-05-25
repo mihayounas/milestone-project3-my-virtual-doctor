c14cb4ac4206f699e9286c68f3a55a4682436d5a
mihayounas <mihaelayounas55@gmail.com>
2022-05-25 10:06:20 +0000

    Add welcome and request name data from the patients

--- run.py
+++ run.py
@@ -21,37 +21,20 @@ def get_details_data():
 
     print('Welcome to My Virtual Doctor !')
     print('\n An app which helps you book your doctor appointments fast!\n')
-    print('To use this app, hit enter after each choice.\n')
+    print('To use this app, press enter after each choice.\n')
     print('After confirming all your details you will receive\n')
     print('a confirmation email!\n')
-    
-    name_str = input('Please enter you full name : ')
-    name_data = name_str.split(" ")
-    while True:
-        if name_data == type(name_data):
-            return name_data
-        else:
-           
-    validate_data(name_data)
+    name = input('Please enter your full name :')
+    try:
+        name.split()[1]
+        print(f"Hello, {name}!")
+    except IndexError:
+        print("Sorry you will need to enter minimum two names...")
 
     # birth_str = input("Please enter your date of birth 00/00/0000:\n")
     # print(f"Born On {birth_str}\n")
     # birth_data = birth_str.split("/")
     # validate_data(birth_data)
-   
 
-def validate_data(values):
-    """
-    Inside this function we will check if there will be minimum 2 strings
-    to confirm the full name and if the date is in this format 00/00/0000.
-    """
-    try:
-        if len(values) != 2:
-            raise ValueError(
-                f"Minimum 2 names required you have entered {len(values)}"
-            )
-    except ValueError as e:
-        print(f'Invalid data: {e}, please try again.\n')    
-
-
+    
 get_details_data()
